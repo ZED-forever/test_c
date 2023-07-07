500bc44735326834b8125777b53fada6faf7b9da
yangjin <g2609597298@gmail.com>
2023-07-07 11:58:15 +0800

    修改冒泡排序的flag

--- helloworld.c
+++ helloworld.c
@@ -282,10 +282,10 @@
 
 // //冒泡排序
 // void bubble_sort(int* arr,int sz){
-//     int flag = 1;
 //     int i = 0;
 //     for(i = 0;i < sz-1;i++){
 //         int j = 0;
+//         int flag = 1;
 //         for(j = 0;j < sz-i-1;j++){
 //             if( arr[j] > arr[j+1]){
 //                 int tmp = 0;
